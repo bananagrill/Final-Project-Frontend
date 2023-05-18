18832f79024cd6101c2d9e98d4e573398826ce2c
Phuvanach Phoemphol <arrmph@gmail.com>
2023-05-18 21:42:16 +0700

    Change API

--- frontend/methond.js
+++ frontend/methond.js
@@ -1,15 +1,15 @@
+const backendIPAddress = "127.0.0.1:3000";
 
 const getDatafromShadow = async () => {
-    const options = {
+     const options = {
         method: "GET",
         headers: { 'Authorization': 'Device 4513aa93-b88c-4e9b-92d3-11ba2920a2ee:Z7XT8mdToAGAYFU8898Xym7sgyBiC64K' },
         credentials: "include",
     };
-    await fetch(`https://localhost:3000/v2/device/shadow/data`, options)
+    await fetch(`http://${backendIPAddress}/api/device/shadow/data`, options)
         .then((response) => response.json())
         .then((data) => {
             console.log(data.data)
         })
         .catch((error) => console.error(error));
 };
-getDatafromShadow()
